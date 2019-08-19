f5dc93c064b5f48b5e429c0f934ec9344e6e74dd
Kannan <kannanlearningarea@gmail.com>
2019-08-19 22:41:06 +0530

    Done

--- webapp/src/app/server/server.component.ts
+++ webapp/src/app/server/server.component.ts
@@ -5,5 +5,9 @@ import { Component } from '@angular/core';
      templateUrl:'./server.component.html'
 })
 export class ServerComponent {
-
+     serverId = 10;
+     status = 'Offline';
+     GetStatus() {
+          return this.status;
+     }
 }
