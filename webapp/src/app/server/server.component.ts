c2df5c2c88577bed22526a2ab7835762bb5ecc02
Kannan <kannanlearningarea@gmail.com>
2019-08-20 21:14:50 +0530

    Clean up

--- webapp/src/app/server/server.component.ts
+++ webapp/src/app/server/server.component.ts
@@ -7,21 +7,8 @@ import { Component } from '@angular/core';
 export class ServerComponent {
      serverId = 10;
      status = 'Offline';
-     allowAddNewServer = false;
-     serverCreationStatus = 'No server was created';
-     serverName = '';
-     serverCreated = false;
      constructor() {
-          setTimeout(() => { this.allowAddNewServer = true; }, 2000);
-     }
-     GetStatus() {
-          return this.status;
-     }
-     onCreateServer() {
-          this.serverCreated = true;
-          this.serverCreationStatus = 'Server was created!';
-     }
-     onUpdateServerName(event: Event ) {
-          this.serverName = (event.target as HTMLInputElement).value;
+          this.serverId = Math.floor(Math.random() * 6) + 1;
+          this.status = Math.random() > 0.5 ? 'Online' : 'Offline';
      }
 }
---
+++ webapp/src/app/servers/servers.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+     selector: 'app-servers',
+     templateUrl: './servers.component.html'
+})
+export class ServersComponent {
+     serverId = 10;
+     status = 'Offline';
+     allowAddNewServer = false;
+     serverCreationStatus = 'No server was created';
+     serverName = '';
+     serverCreated = false;
+     constructor() {
+          this.serverId = Math.floor(Math.random() * 6) + 1;
+          setTimeout(() => { this.allowAddNewServer = true; }, 2000);
+     }
+     GetStatus() {
+          return Math.random() > 0.5 ? 'Online' : 'Offline';
+     }
+     onCreateServer() {
+          this.serverCreated = true;
+          this.serverCreationStatus = 'Server was created!';
+     }
+     onUpdateServerName(event: Event ) {
+          this.serverName = (event.target as HTMLInputElement).value;
+     }
+}
