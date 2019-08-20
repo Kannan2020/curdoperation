c3f14353fe5fd50b5b6241a6f12ad3db778b3b13
Kannan <kannanlearningarea@gmail.com>
2019-08-20 20:08:58 +0530

    Done

--- webapp/src/app/server/server.component.ts
+++ webapp/src/app/server/server.component.ts
@@ -2,16 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
      selector: 'app-server',
-     templateUrl:'./server.component.html'
+     templateUrl: './server.component.html'
 })
 export class ServerComponent {
      serverId = 10;
      status = 'Offline';
      allowAddNewServer = false;
+     serverCreationStatus = "No server was created";
      constructor() {
-          setTimeout(() => { this.allowAddNewServer=true },2000)
+          setTimeout(() => { this.allowAddNewServer = true }, 2000)
      }
      GetStatus() {
           return this.status;
+     }
+     onCreateServer() {
+          this.serverCreationStatus = "Server was created!";
      }
 }
